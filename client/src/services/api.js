ad5d4f3442216d44a3fa41ad154f694148504167
Tarun Kumar <tarunkumarmanjhi20@gmail.com>
2026-05-24 09:27:45 +0530

    Connect frontend to deployed backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api"
+  baseURL: "https://transport-management-system-rr33.onrender.com/api"
 });
 
 API.interceptors.request.use((config) => {
